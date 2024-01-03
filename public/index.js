000471a71748a94e8b0a603db5661cefc6252853
Denimwearer <CorderoDixon12@yahoo.com>
2024-01-03 15:43:51 -0500

    added counter to idea

--- public/index.js
+++ public/index.js
@@ -1,1 +1,23 @@
-console.log("Linked");
+const textArea = document.getElementById("idea");
+const characterCounter = document.getElementById("character-counter");
+const typedCharacters = document.getElementById("typed-characters");
+const maxCharacters = 50;
+const createIdea = document.getElementById("create-idea");
+const form = document.querySelector("form");
+
+textArea.addEventListener("keyup", (e) => {
+  const typedCharactersLength = textArea.value.length;
+  if (typedCharactersLength > maxCharacters) {
+    return false;
+  }
+
+  typedCharacters.textContent = typedCharactersLength;
+});
+
+createIdea.addEventListener("click", () => {
+  if (form.style.display === "none" || form.style.display === "") {
+    form.style.display = "inline";
+  } else {
+    form.style.display = "none";
+  }
+});
